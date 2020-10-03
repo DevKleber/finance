51e26ec44ef0a232ec4c6784b7dac73ccf34ba8d
kleber <kleber107@gmail.com>
2020-10-03 01:03:13 -0300

    notify error

--- frontend/src/app/categoria-despesa/categoria-despesa.service.ts
+++ frontend/src/app/categoria-despesa/categoria-despesa.service.ts
@@ -1,72 +1,71 @@
-import { Injectable } from '@angular/core'
-import { Observable } from 'rxjs'
-import { tap, filter } from 'rxjs/operators'
-import { HttpClient } from '@angular/common/http'
-import { Router, NavigationEnd } from '@angular/router'
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { tap, filter } from "rxjs/operators";
+import { HttpClient } from "@angular/common/http";
+import { Router, NavigationEnd } from "@angular/router";
 
-import { CategoriaDespesa } from './categoria-despesa.model'
+import { CategoriaDespesa } from "./categoria-despesa.model";
 
-
-import { NotificationService } from '../shared/messages/notification.service';
-import { API } from '../app.api'
+import { NotificationService } from "../shared/messages/notification.service";
+import { API } from "../app.api";
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: "root",
 })
-
 export class CategoriaDespesaService {
+	constructor(
+		private http: HttpClient,
+		private notificationService: NotificationService,
+		private router: Router
+	) {}
 
-  constructor(
-    private http: HttpClient,
-    private notificationService: NotificationService,
-    private router: Router
-  ) { }
+	getCategoriaDespesas(search?: string): Observable<CategoriaDespesa[]> {
+		return this.http.get<CategoriaDespesa[]>(`${API}/categoria-despesa`);
+	}
 
-  getCategoriaDespesas(search?: string): Observable<CategoriaDespesa[]> {
+	getcategoriaDespesaById(id: string): Observable<CategoriaDespesa> {
+		return this.http.get<CategoriaDespesa>(
+			`${API}/categoria-despesa/${id}`
+		);
+	}
 
-    return this.http.get<CategoriaDespesa[]>(`${API}/categoria-despesa`)
-  }
+	save(form) {
+		return this.http.post<any>(`${API}/categoria-despesa`, form).subscribe(
+			(data) => {
+				if (data["dados"]) {
+					this.notify("Registro Inserido Com Sucesso!");
+					this.router.navigate(["/categoria-despesa"]);
+				}
+				console.log(data);
+			},
+			(error) => {
+				this.notify(`Error: ${error}`);
+			}
+		);
+	}
 
+	update(form, id) {
+		return this.http.put(`${API}/categoria-despesa/${id}`, form).subscribe(
+			(data) => {
+				if (data["response"]) {
+					this.notify("Registro Alterado Com Sucesso!");
+					this.router.navigate(["/categoria-despesa"]);
+				}
+				console.log(data);
+			},
+			(error) => {
+				this.notify(`Error: ${error}`);
+			}
+		);
+	}
+	inativar(id: string) {
+		return this.http.delete(`${API}/categoria-despesa/${id}`);
+	}
 
-  getcategoriaDespesaById(id: string): Observable<CategoriaDespesa> {
-    return this.http.get<CategoriaDespesa>(`${API}/categoria-despesa/${id}`)
-
-  }
-
-  save(form) {
-    return this.http.post<any>(`${API}/categoria-despesa`, form)
-    .subscribe((data) => {
-        if (data['dados']) {
-          this.notify('Registro Inserido Com Sucesso!');
-          this.router.navigate(['/categoria-despesa'])
-        }
-        console.log(data);
-      }, (error) => {
-        this.notify(`Error: ${error}`);
-      });
-    }
-
-  update(form,id) {
-    return this.http.put(`${API}/categoria-despesa/${id}`, form)
-    .subscribe((data) => {
-      if (data['response']) {
-        this.notify('Registro Alterado Com Sucesso!');
-        this.router.navigate(['/categoria-despesa'])
-      }
-      console.log(data);
-    }, (error) => {
-      this.notify(`Error: ${error}`);
-    });
-  }
-  inativar(id: string) {
-    return this.http.delete(`${API}/categoria-despesa/${id}`)
-
-  }
-
-  notify(msg) {
-    this.notificationService.notify(msg);
-  }
-  goTo(path: string = 'depoimento') {
-    this.router.navigate([`/${path}`])
-  }
+	notify(msg) {
+		this.notificationService.notifySweet(msg);
+	}
+	goTo(path: string = "depoimento") {
+		this.router.navigate([`/${path}`]);
+	}
 }
