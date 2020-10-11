4af847b82a9cfad95758324b58a54e39b7217244
kleber <kleber107@gmail.com>
2020-10-11 14:49:38 -0300

    colocando icone no menu

--- api/app/Http/Controllers/MenuController.php
+++ api/app/Http/Controllers/MenuController.php
@@ -14,54 +14,63 @@ class MenuController extends Controller
      */
     private $token;
 
-    public function __construct() 
+    public function __construct()
     {
         $this->token = JWTAuth::parseToken()->authenticate();
     }
+
     public function index()
     {
-        $menu = \App\Menu::Join('sistema_rotina', 'sistema_modulo.cd_sistema_modulo', '=', 'sistema_rotina.cd_sistema_modulo')
-        ->select('sistema_rotina.*','sistema_modulo.*')
-        ->where('sistema_modulo.bo_ativo',1)
-        ->get();
+        return \App\Menu::Join('sistema_rotina', 'sistema_modulo.cd_sistema_modulo', '=', 'sistema_rotina.cd_sistema_modulo')
+            ->select('sistema_rotina.*', 'sistema_modulo.*')
+            ->where('sistema_modulo.bo_ativo', 1)
+            ->orderBy('ordem')
+            ->get()
+        ;
         // $menu = \App\Menu::leftJoin('sistema_rotina', 'sistema_modulo.cd_sistema_modulo', '=', 'sistema_rotina.cd_sistema_modulo')
         // ->select('sistema_rotina.*','sistema_modulo.*')
         // ->where('sistema_modulo.bo_ativo',1)
         // ->get();
-        return $menu;
         // return [$this->buildTree($menu->toArray())];
     }
-    public function getMenus(){
-        
+
+    public function getMenus()
+    {
     }
-   
-    public function processarMenu($ar){
-        $array = array();
+
+    public function processarMenu($ar)
+    {
+        $array = [];
         $cont = 0;
-        foreach($ar as $key => $value){
-            $array[$value['no_modulo'].'_'.$value['cd_sistema_modulo']][] = array(
-                'cd_sistema_rotina'=>$value['cd_sistema_rotina'],
-                'ds_rotina'=>$value['ds_rotina'],
-                'no_rotina'=>$value['no_rotina'],
-                'icone'=>$value['icone'],
-                'ds_url'=>$value['ds_url']
-            );
-           $cont++;
+        foreach ($ar as $key => $value) {
+            $array[$value['no_modulo'].'_'.$value['cd_sistema_modulo']][] = [
+                'cd_sistema_rotina' => $value['cd_sistema_rotina'],
+                'ds_rotina' => $value['ds_rotina'],
+                'no_rotina' => $value['no_rotina'],
+                'icone' => $value['icone'],
+                'ds_url' => $value['ds_url'],
+            ];
+            ++$cont;
         }
-        
+
         return json_decode(json_encode($array), true);
     }
-    function buildTree($items) {
 
-        $childs = array();
-    
-        foreach($items as $item)
+    public function buildTree($items)
+    {
+        $childs = [];
+
+        foreach ($items as $item) {
             $childs[$item['cd_sistema_modulo']][] = $item;
-    
-        foreach($items as $item) if (isset($childs[$item['cd_sistema_rotina']]))
-            $item['childs'] = $childs[$item['cd_sistema_rotina']];
-    
-        return (array)$childs;
+        }
+
+        foreach ($items as $item) {
+            if (isset($childs[$item['cd_sistema_rotina']])) {
+                $item['childs'] = $childs[$item['cd_sistema_rotina']];
+            }
+        }
+
+        return (array) $childs;
     }
 
     /**
@@ -71,62 +80,58 @@ class MenuController extends Controller
      */
     public function create()
     {
-        //
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
-        //
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
     {
-        //
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param int $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
     {
-        //
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
-        //
     }
 }
