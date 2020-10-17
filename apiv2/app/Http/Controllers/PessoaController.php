6667ee5d1bd417cdb072a3af50f9863222f8f9af
kleber <kleber107@gmail.com>
2020-10-17 20:10:01 -0300

    user 17 categoria

--- apiv2/app/Http/Controllers/PessoaController.php
+++ apiv2/app/Http/Controllers/PessoaController.php
@@ -29,7 +29,7 @@ class PessoaController extends Controller
 
     public function criarCategoriaParaUsuario()
     {
-        $this->categoriaDespesas(22);
+        $this->categoriaDespesas(17);
     }
 
     /**
