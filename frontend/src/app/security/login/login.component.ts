41a5707a1a5833499c44b5c75357ac2be1c06a09
kleber <kleber107@gmail.com>
2020-12-27 17:33:21 -0300

    build

--- frontend/src/app/security/login/login.component.ts
+++ frontend/src/app/security/login/login.component.ts
@@ -81,7 +81,7 @@ export class LoginComponent implements OnInit, AfterContentInit {
 					//enviando para ultima rota.
 					// location.replace("/#" + atob(this.navigateTo))
 					// location.replace("/#/dashboard");
-					document.location.href = "/#/dashboard/";
+					window.location.href = "/#/dashboard/";
 					return false;
 
 					// this.notificationService.notifySweet(`Bem vindo, ${user.user["no_funcionario"]}`),
